f7a218942e92b1a8daa5a7ccbc69f9b4b6f73fbd
nyel-101 <dandevjr@gmail.com>
2025-03-05 19:16:16 +0000

    fix(auth): restructure view templates

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c21f7c182c0d429f86cf33bca5afad73 = () => {
-  return '2026-02-04T18:17:26Z';
+const node_1699e8eb50734731b4bde05655a66f91 = () => {
+  return '2025-03-05T19:16:16Z';
 };
