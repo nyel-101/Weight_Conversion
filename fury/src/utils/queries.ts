15b3b47fb44c4db8941f74587451ef7ea9002010
nyel-101 <dandevjr@gmail.com>
2025-03-25 14:25:02 +0000

    refactor(deps): update view templates

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c9e769aa694e4bd68f18cc08cddecba9 = () => {
-  return '2026-06-11T20:28:26Z';
+const node_6b91395fe28d45d09ebce12485b3601c = () => {
+  return '2025-03-25T14:25:02Z';
 };
