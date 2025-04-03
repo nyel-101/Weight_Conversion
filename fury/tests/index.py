90af350d389b448e23b8d334d0b4e32dd2acb0cf
nyel-101 <dandevjr@gmail.com>
2025-04-03 13:59:49 +0000

    test(ui): update session store

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5e4993e4068144f7bd2ab5ff46a88b50 = () => {
-  return '2025-02-23T23:39:38Z';
+const node_2c8c2d8055df4056b8d529493e4c7566 = () => {
+  return '2025-04-03T13:59:49Z';
 };
