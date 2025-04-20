46fddd58fa8dc420ed3751c8c8d9208bfae92762
nyel-101 <dandevjr@gmail.com>
2025-04-20 07:58:44 +0000

    refactor(config): integrate auth tokens

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6cba6762835843e7ba478613beadd98d = () => {
-  return '2025-04-02T14:31:05Z';
+const node_61aeb5d4f61240abb718d3de6472141b = () => {
+  return '2025-04-20T07:58:44Z';
 };
