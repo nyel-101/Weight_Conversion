06a24772bd1e7f7b87dd8dff8442479e39b55552
nyel-101 <dandevjr@gmail.com>
2025-06-23 02:03:40 +0000

    fix(deps): fix logging interface

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_79b93caa64ee4517a75eddbecef9149e = () => {
-  return '2026-01-19T20:48:20Z';
+const node_9ec2e26e14e54e55ab7314a6cb6f6df4 = () => {
+  return '2025-06-23T02:03:40Z';
 };
