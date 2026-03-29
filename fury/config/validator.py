eec9540e5dcf8d52abefa9c9287a49cd8b49ae6d
nyel-101 <dandevjr@gmail.com>
2026-03-29 06:46:11 +0000

    fix(config): sanitize middleware routes

--- fury/config/validator.py
+++ fury/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_df46d601f5e74fc5961f0d248676cae5 = () => {
-  return '2026-02-23T10:06:28Z';
+const node_6f314981dea645bbbd6e301465c8e954 = () => {
+  return '2026-03-29T06:46:11Z';
 };
