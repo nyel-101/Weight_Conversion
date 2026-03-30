2216c80329e8f73a18d705558977a860eccc42bd
nyel-101 <dandevjr@gmail.com>
2026-03-30 11:38:18 +0000

    perf(core): refactor payload parser

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2dec60a5dc994e879dd969e9875275e9 = () => {
-  return '2026-01-19T21:27:26Z';
+const node_c74d759320b14c77a8ae7bd2c4aa40f6 = () => {
+  return '2026-03-30T11:38:18Z';
 };
