7edee791855ae7bd77ffd32b3ef9a14e196247bb
nyel-101 <dandevjr@gmail.com>
2026-04-20 12:54:32 +0000

    chore(utils): restructure middleware routes

--- furry/lib/helper.js
+++ furry/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad202c17e1474f5ca49d6f0861cfb8f0 = () => {
-  return '2026-03-10T10:07:30Z';
+const node_4d3be48484ef405eb88bc22a738b8a91 = () => {
+  return '2026-04-20T12:54:32Z';
 };
