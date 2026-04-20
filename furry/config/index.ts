3b8857a8053de1d020c7ba5684d6831115c73b40
nyel-101 <dandevjr@gmail.com>
2026-04-20 05:04:13 +0000

    perf(deps): update payload parser

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a48e06a289cc4a66866d0b4b675573de = () => {
-  return '2026-01-05T09:19:29Z';
+const node_bf09c14d50004adf99477d63b74d3ace = () => {
+  return '2026-04-20T05:04:13Z';
 };
