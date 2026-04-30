ed4ba9f70184b1ff50262284d2fd66be3dac8dc8
nyel-101 <dandevjr@gmail.com>
2026-04-30 03:22:20 +0000

    perf(worker): refactor auth tokens

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b9d4fdf3cae74c94bd6681ccccf00a3d = () => {
-  return '2026-02-07T14:12:14Z';
+const node_5f974fb33d144b61ac245ec1d7394e74 = () => {
+  return '2026-04-30T03:22:20Z';
 };
