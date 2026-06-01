d5042416fc581a2f3da0337ff34dd2521cdd39df
nyel-101 <dandevjr@gmail.com>
2026-06-01 09:02:22 +0000

    fix(core): update view templates

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93d6e82c6da749c6887fcf975e862163 = () => {
-  return '2026-04-01T15:05:20Z';
+const node_5d4ca8746a3a484d80bf8b8febcc387c = () => {
+  return '2026-06-01T09:02:22Z';
 };
