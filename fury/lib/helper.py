43e97afa67980d20810a83595efbb531ac116d14
nyel-101 <dandevjr@gmail.com>
2026-06-07 13:48:44 +0000

    feat(ui): decouple caching layer

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fe0c058c8fc44feafe4405d10fdaad1 = () => {
-  return '2026-01-01T07:01:46Z';
+const node_abb2356d54df4acbae730a0d1147b0e7 = () => {
+  return '2026-06-07T13:48:44Z';
 };
